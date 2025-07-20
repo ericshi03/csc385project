a6533c932b4868842169e74992e35ceaca2d30d5
eric <ericshi97@gmail.com>
2025-07-20 12:08:04 -0400

    added logging sstatements

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include "mbed.h"
 
 #include "include/light_sensor.h"
+#include "include/hum_sensor.h"
 
 
 void setup() {
     light_init();
-
+    humid_init();
 }
 
 int main() {
     setup();
 
+    bool swap_light_humidity = true;
+
     while (true) {
-        float lux = read_light();
-        printf("Light sensor (BH1750) lux: %.2f\n", lux);
+        if (swap_light_humidity) {
+            float humidity = read_humidity();
+            printf("Humidity sensor (SHT31) humidity: %.2f%%\n", humidity);
+        } else {
+            float lux = read_light();
+            printf("Light sensor (BH1750) lux: %.2f\n", lux);
+        }
+
+
+
+
         ThisThread::sleep_for(500ms);
     }
 }
