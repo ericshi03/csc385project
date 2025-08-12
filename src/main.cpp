97030ba06e7a62d528dc685dc2cf0af457652f2c
eric <ericshi97@gmail.com>
2025-08-12 13:42:55 -0400

    lets test

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include "../include/light_sensor.h"
 #include "../include/hum_sensor.h"
 #include "../include/temp_sensor.h"
-#include "../include/mqtt_manager.h"
+#include "../include/mqtt_manager_core.h"
+
+#define CURRENT_MACHINE "sensor_hub"
 
 I2C i2c(I2C_SDA, I2C_SCL);
 
@@ -13,6 +15,19 @@ void setup() {
     //empty for now
     humid_init();
     temp_init();
+    //setup wifi here
+
+    WiFiInterface *wifi = WiFiInterface::get_default_instance();
+
+    mqtt_init_client(wifi, "your_ssid", "your_password", "test.mosquitto.org", 1883, CURRENT_MACHINE);
+
+    if (CURRENT_MACHINE == "server_hub") {
+        //subscribe to topics
+        mqtt_subscribe("sensorhub/temperature");
+        mqtt_subscribe("sensorhub/humidity");
+        mqtt_subscribe("sensorhub/light");
+
+    }
 }
 
 int main() {
@@ -25,15 +40,19 @@ int main() {
             float humidity = read_humidity();
             char buf[16];
             snprintf(buf, sizeof(buf), "%.2f", humidity);
+            mqtt_publish("sensorhub/humidity", buf);
 
             float temperature = read_temperature(); 
             snprintf(buf, sizeof(buf), "%.2f", temperature);
+            mqtt_publish("sensorhub/temperature", buf);
 
             float lux = read_light();
             snprintf(buf, sizeof(buf), "%.2f", lux);
+            mqtt_publish("sensorhub/light", buf);
 
         }
+        mqtt_yield(100);
 
-        ThisThread::sleep_for(500ms);
+        ThisThread::sleep_for(100ms);
     }
 }
